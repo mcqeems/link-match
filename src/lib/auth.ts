a95d998fb54e80639870e507c0a68d5decbbb2b9
mcqeems <mcqeemsofficial@gmail.com>
2025-09-21 00:00:34 +0700

    added files for basic auth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,7 @@
 import { betterAuth } from 'better-auth';
 import { prismaAdapter } from 'better-auth/adapters/prisma';
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '@/generated/prisma';
+import { nextCookies } from 'better-auth/next-js';
 
 const prisma = new PrismaClient();
 
@@ -8,4 +9,8 @@ export const auth = betterAuth({
   database: prismaAdapter(prisma, {
     provider: 'mysql',
   }),
+  emailAndPassword: {
+    enabled: true,
+  },
+  plugins: [nextCookies()],
 });
