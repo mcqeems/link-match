e8c3834e4ce78f5e7a4cbfad9106341c461f0728
mcqeems <mcqeemsofficial@gmail.com>
2025-10-02 13:29:58 +0700

    navbar magic matcher hidden for talenta

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -68,9 +68,12 @@ export default async function Navbar() {
               <li>
                 <Link href="/talents">Cari Talenta</Link>
               </li>
-              <li>
-                <Link href="/magic-matcher">Magic Matcher</Link>
-              </li>
+              {profileRole === 'Recruiter' && (
+                <li>
+                  <Link href="/magic-matcher">Magic Matcher</Link>
+                </li>
+              )}
+
               <li>
                 <Link href="/messages">Messages</Link>
               </li>
